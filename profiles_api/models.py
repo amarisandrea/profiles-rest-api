f5d55c3f435e60ab712c3771fb0dc4c95612b067
Andrea <ac.ramirez11@uniandes.edu.co>
2021-03-10 08:11:22 -0500

    additional seetings

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -4,32 +4,25 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 class UserProfileManager(BaseUserManager):
-    """Manager for user profiles"""
-    def create_user(self,email,name,password=None):
-      """Create  a new user profiles"""
-      if not email:
+   """Manager for user profiles"""
+   def create_user(self,email,name,password=None):
+       """Create  a new user profiles"""
+       if not email:
           raise ValueError( 'User must have an email address')
 
-      email = self.normalize_email(email)
-      user = self.model(email=email, name=name)
+       email = self.normalize_email(email)
+       user = self.model(email=email, name=name)
+       user.set_password(password)
+       user.save(using=self._db)
+       return user
 
-      user.set_password(password)
-      user.save(using=self._db)
-
-      return user
-
-    def create_superuser(self, email, name, password):
-        """Create and save a new superuser with given details"""
-        user= self.create_user(email,name,password)
-
-        user.is_superuser= True
-        user.is_staff= True
-        user.save(using=self._db)
-
-        return user
-
-
-
+   def create_superuser(self, email, name, password):
+       """Create and save a new superuser with given details"""
+       user= self.create_user(email,name,password)
+       user.is_superuser= True
+       user.is_staff= True
+       user.save(using=self._db)
+       return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
    """Database model for users in the system"""
@@ -50,5 +43,5 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
        """Retrieve short name of user"""
        return self.name
    def __str__(self):
-        """Retrieve  string represation od our users"""
-       return self.email
+      """Retrieve  string represation od our users"""
+      return self.email
